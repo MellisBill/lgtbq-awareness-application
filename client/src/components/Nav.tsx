4bf93cf7f9ee5b5e9101ab32c5c4b7ad2090c650
Billy Mellis <mellisbill@gmail.com>
2023-03-07 16:32:10 +0000

    Updated nav component

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { menu_items } from '../data/menu-items'
+import Logo from '../assets/Logo.png'
 
 export const Nav = () => {
   return (
-    <nav>
-        <h1>CGI LGBT+ Hub</h1>
-        <ul>
-            {menu_items.map((item) => {
-                return (
-                <li key={item.value}>
-                    <Link className='nav-link' to={item.value}>{item.label}</Link>
-                </li>)
-            })}
+    <nav className='navbar'>
+      <div className='navbar-content'>
+
+         <img src={Logo} alt="" />
+        <ul className='nav-links'>
+          {menu_items.map((item) => {
+            return (
+              <li key={item.value}>
+                <Link className='nav-link' to={item.value}>{item.label}</Link>
+              </li>)
+          })}
         </ul>
+          <button className='ask-question'>
+          Ask a question
+          </button>
+      </div>
+       
     </nav>
+
   )
 }
