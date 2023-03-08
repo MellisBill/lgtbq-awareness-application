59c2fd35463b9b0809e96083cdf943fee4c30703
Kim Roberts <kim.roberts@live.co.uk>
2023-03-08 13:18:30 +0000

    Code refactoring

--- client/src/components/FormAcceptModal.tsx
+++ client/src/components/FormAcceptModal.tsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+
 export const FormAcceptModal = (props: any) => {
 
-  const { setOpen } = props;
+  const { setOpen, content, storageKey } = props;
   const [accepted, setAccepted] = useState(false);
-
 
   const onContinue = () => {
     if (accepted) {
-      localStorage.setItem('accepted', 'true');
+      localStorage.setItem(`accepted-${storageKey}`, 'true');
       setOpen(false);
     }
   }
 
   return <>
   <h2>Terms and Conditions</h2>
-    <p className='terms-content'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium laboriosam doloremque quod vero, modi possimus id iste. Molestiae totam ratione culpa veritatis, sunt ad iste praesentium aliquid, iusto reprehenderit eveniet? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi quisquam fuga architecto! Alias, exercitationem dolore? Fuga ullam officia dicta? Nisi maxime mollitia, officia veritatis minus quis laudantium fugiat ut molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam incidunt quo esse beatae doloremque ex nobis rem totam, harum dolore aliquam. Nesciunt error quas ducimus explicabo suscipit, ratione nisi assumenda! Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam aliquam aliquid placeat cum sit reprehenderit, aspernatur corporis quas esse quam corrupti eius! Perspiciatis minima impedit veritatis vitae cupiditate ex quod recusandae voluptatum vero dicta voluptatibus ab temporibus veniam laboriosam maiores, eligendi modi! Ullam, est recusandae iusto unde repudiandae voluptatum distinctio tenetur adipisci repellat exercitationem quidem, voluptatem labore nesciunt sunt. Iusto mollitia quidem atque unde molestias, tempora explicabo tenetur, quaerat libero, nulla rerum. Provident inventore facilis aliquam consequatur ea id distinctio. Quasi dolor corporis porro iste neque nulla doloremque! Velit excepturi mollitia maxime? Culpa aspernatur ipsum est iusto atque dolore totam.</p>
+    <p className='terms-content'>{content}</p>
     <form className='accept-terms-form' onSubmit={(e => e.preventDefault())}>
-      <div className='accept-check-container'>
+      <div className='check-box-container'>
         <button className='button-no-style' onClick={() => setAccepted(!accepted)}>
           <span className="material-symbols-outlined">
             { accepted ? 'check_box' : 'check_box_outline_blank' }
--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, { ReactElement, ReactNode } from 'react'
 import { useState, useEffect } from 'react';
 import { FormAcceptModal } from './FormAcceptModal';
 
-export const Modal = (props: any) => {
+export const Modal = ({children, storageKey}: any) => {
 
-    const { children } = props
+    // state reflecting if modal is visible or not
     const [open, setOpen] = useState(true);
 
-    useEffect(() => {
-        if (localStorage.getItem('accepted')) {
-          setOpen(false);
-        };
-      }, []);
+    // clone children woth props passed to them 
+    const childrenWithProps = React.Children.map<ReactElement, ReactElement>(children, child => {
+        const newProps = {
+            setOpen: setOpen
+        }
+        if (React.isValidElement(child)) {
+          return React.cloneElement(child, newProps);
+        }
+        return child;
+      });
     
     return <>
         {open && <div>
             <div className='modal-backdrop'/>
             <div className='modal-content'>
-                <FormAcceptModal setOpen={setOpen}/>
+                {childrenWithProps}
             </div>
         </div>}
     </>  
