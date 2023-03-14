b123a0ba1141d9221bdfa96212892ecafd4c3367
Kim Roberts <kim.roberts@live.co.uk>
2023-03-14 15:43:54 +0000

    Tidied up imports

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react'
-import { Toggle } from '../components/Toggle'
+import React from 'react'
 
 export const Home = () => {
 
--- client/src/pages/Links.tsx
+++ client/src/pages/Links.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { EmbeddedForm } from '../components/EmbeddedForm'
-import { url } from '../data/question-form';
 
 export const Links = () => {
   return (
