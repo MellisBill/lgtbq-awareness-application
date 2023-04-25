7c476536b01f231ce06dafb04a875f5dcdc1a1e7
Kim Roberts <kim.roberts@live.co.uk>
2023-04-25 16:10:21 +0100

    Added tag functionality

--- client/src/components/QuestionAnswer.tsx
+++ client/src/components/QuestionAnswer.tsx
@@ -1,26 +1,45 @@
-import React from 'react'
-import { formConfig } from '../config/google-forms';
-import { extractAnswer } from '../lib/utils';
-import { FormAPIResponseItem } from '../types/types';
+import React from "react";
+import { formConfig } from "../config/google-forms";
+import { extractAnswer, extractTags } from "../lib/utils";
+import { FormAPIResponseItem } from "../types/types";
 
-export const QuestionAnswer = ({responseItem}: {responseItem: FormAPIResponseItem}) => {
-//export const QuestionAnswer = () => {
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+export const QuestionAnswer = ({responseItem} : {responseItem: FormAPIResponseItem}) => {
+  //export const QuestionAnswer = () => {
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+  />;
 
-  const question = extractAnswer(responseItem, formConfig.responseForm.questionIds[0])
-  const answer = extractAnswer(responseItem, formConfig.responseForm.questionIds[1])
+  const question = extractAnswer(
+    responseItem,
+    formConfig.responseForm.questionIds[0]
+  );
+  const answer = extractAnswer(
+    responseItem,
+    formConfig.responseForm.questionIds[1]
+  );
+  const tags = extractTags(
+    extractAnswer(responseItem, formConfig.responseForm.questionIds[2])
+  );
 
   return (
-    <div className='question-response'>
-      <div className='question-response-asker-container'>
-        <p><span className="material-symbols-outlined">person</span><div className='question-response-asker'>Anonymous</div></p>
+    <div className="question-response">
+      <div className="question-response-asker-container">
+        <p>
+          <span className="material-symbols-outlined">person</span>
+          <div className="question-response-asker">Anonymous</div>
+        </p>
       </div>
-      <p className='question-response-question'><b>{question}</b></p>
+      <p className="question-response-question">
+        <b>{question}</b>
+      </p>
       <p></p>
-      <p className= 'question-response-tags'>LGBT</p><p className= 'question-response-tags'>Gender</p>
-      <div className='question-response-answer'>
+      {tags.map((tag, i) => {
+        return <p className="question-response-tags">{tag}</p>;
+      })}
+      <div className="question-response-answer">
         <p>{answer}</p>
       </div>
     </div>
-  )
-}
+  );
+};
