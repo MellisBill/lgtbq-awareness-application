f7a1bcb7c881d562207befea2bdf6b37b61de526
Kim Roberts <kim.roberts@live.co.uk>
2023-04-25 13:38:50 +0100

    Completed carousel component

---
+++ client/src/data/home-page.ts
@@ -0,0 +1,26 @@
+export const pageData = [
+  {
+    title: "Our mission",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Utatque, sint consequuntur alias neque ea eius perspiciatis quaerat. Inventore eum quisquam dolorum. Inventore sed sit dolore enim fuga, hic, soluta eos veritatis laborum nobis ratione consequuntur modi doloribus provident harum similique est. Vel, quae culpa! Vel quos tempore necessitatibus beatae.",
+    imageSrc: require('../assets/images/pride.jpeg'),
+  },
+  {
+    title: "Links to useful policy",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Utatque, sint consequuntur alias neque ea eius perspiciatis quaerat. Inventore eum quisquam dolorum. Inventore sed sit dolore enim fuga, hic, soluta eos veritatis laborum nobis ratione consequuntur modi doloribus provident harum similique est. Vel, quae culpa! Vel quos tempore necessitatibus beatae.",
+      imageSrc: require('../assets/images/links.jpeg'),
+  },
+  {
+    title: "Anonymous Q&A",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Utatque, sint consequuntur alias neque ea eius perspiciatis quaerat. Inventore eum quisquam dolorum. Inventore sed sit dolore enim fuga, hic, soluta eos veritatis laborum nobis ratione consequuntur modi doloribus provident harum similique est. Vel, quae culpa! Vel quos tempore necessitatibus beatae.",
+      imageSrc: require('../assets/images/anonymous.jpeg'),
+  },
+  {
+    title: "Glossary of terms",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Utatque, sint consequuntur alias neque ea eius perspiciatis quaerat. Inventore eum quisquam dolorum. Inventore sed sit dolore enim fuga, hic, soluta eos veritatis laborum nobis ratione consequuntur modi doloribus provident harum similique est. Vel, quae culpa! Vel quos tempore necessitatibus beatae.",
+      imageSrc: require('../assets/images/glossary.jpeg'),
+  },
+];
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,27 +1,25 @@
-import React from 'react'
-import { PageHeading } from '../components/PageHeading'
-import { Carousel } from '../components/Carousel'
-import { CarouselItem } from '../components/CarouselItem'
-import { InfoSlide } from '../components/InfoSlide'
-import pride from "../resources/images/pride.jpeg";
+import React from "react";
+import { PageHeading } from "../components/PageHeading";
+import { Carousel } from "../components/Carousel";
+import { CarouselItem } from "../components/CarouselItem";
+import { pageData } from "../data/home-page";
+import { InfoSlide } from "../components/InfoSlide";
 
 export const Home = () => {
-  return <>
-    <PageHeading title='Home'/>
-    <Carousel>
-      <CarouselItem>
-        <InfoSlide title='Our mission' alignment='right' imageSrc={pride}>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut atque, sint consequuntur alias neque ea eius perspiciatis quaerat. Inventore eum quisquam dolorum. Inventore sed sit dolore enim fuga, hic, soluta eos veritatis laborum nobis ratione consequuntur modi doloribus provident harum similique est. Vel, quae culpa! Vel quos tempore necessitatibus beatae.</p>
-        </InfoSlide>
-      </CarouselItem>
-      <CarouselItem>
-      <InfoSlide title='Links to useful policy' alignment='left' imageSrc={pride}>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore numquam laboriosam fuga corrupti distinctio ex, earum iure veniam ea unde.</p>
-      </InfoSlide>
-      </CarouselItem>
-      <CarouselItem>
-        <div className='blue'>Item three</div>
-      </CarouselItem>
-    </Carousel>
-  </>
-}
+  return (
+    <>
+      <PageHeading title="Home" />
+      <Carousel>
+        {pageData.map((page, i) => {
+          return (
+            <CarouselItem>
+              <InfoSlide title={page.title} imageSrc={page.imageSrc} alignment={i % 2 == 0 ? 'left' : 'right'}>
+                <p>{page.content}</p>
+              </InfoSlide>
+            </CarouselItem>
+          );
+        })}
+      </Carousel>
+    </>
+  );
+};
