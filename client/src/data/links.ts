27347a035f3cb2eda12b82e28a73f996297c6c88
Kim Roberts <kim.roberts@live.co.uk>
2023-04-25 14:15:19 +0100

    Added links data file

---
+++ client/src/data/links.ts
@@ -0,0 +1,47 @@
+export const links = [
+    {
+        title: 'FPA',
+        description: 'This site can be used to find a local Sexual Health or GUM clinic in your area for everything from STI testing to contraception for both younger and older people. Gender Identity Development Service - A specialised NHS clinic for young people exploring or questioning their gender identity. The website has a number of resources for young people and parents.',
+        url: ''
+    },
+    {
+        title: 'Gendered Intelligence',
+        description: 'A charity that provides awareness training for gender diversity. They have some resources on their website, but primarily they run support groups and facilitate youth camps. They also offer specific support groups and facilitate activities for trans people of colour, trans people of faith, etc.',
+        url: ''
+    },
+    {
+        title: 'LGBT Foundation',
+        description: 'A charity group aimed at helping LGBT people and offers both mental and sexual health support while also organising events while pushing for better protections for LGBT peoples.',
+        url: ''
+    },
+    {
+        title: 'Mermaids',
+        description: 'A charity that raises awareness & campaigns about gender nonconformity in children. There are resources for both teenagers and parents there. Mermaids also have a helpline/ chat feature for either you or the child to contact to talk anything through.',
+        url: ''
+    },
+    {
+        title: 'Mind',
+        description: 'This page has a list of contact numbers for various mental health charities and services such as the Samaritans and CALM.',
+        url: ''
+    },
+    {
+        title: 'Mindline Trans+',
+        description: 'A new national helpline for people who identify as trans, agender, gender fluid, or non-binary. This is provided by the Charity \'Mind\', and they provide a safe space for them to talk about their feelings confidentially.',
+        url: ''
+    },
+    {
+        title: 'NHS Mental Health Services',
+        description: 'This page can be used to find services close to you and provides you with a choice of available local centres and contacts available.',
+        url: ''
+    },
+    {
+        title: 'Stonewall',
+        description: 'An LGBT rights charity that campaigns and lobbys to change laws, transform organisations and to change hearts and minds.',
+        url: ''
+    },
+    {
+        title: 'Switchboard ',
+        description: 'The LGBT+ helpline. They provide an information, support and referral process for lesbians, gay men, and bisexual and trans people.',
+        url: ''
+    },
+]
